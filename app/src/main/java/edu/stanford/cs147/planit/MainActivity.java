e33ae6b06c7e3d2fc663f9b855e59b447058703a
Johnson Song <johnsonsong@DN0a24175d.SUNet>
2018-11-27 09:59:55 -0800

    start on ready-set-go screen

--- app/src/main/java/edu/stanford/cs147/planit/MainActivity.java
+++ app/src/main/java/edu/stanford/cs147/planit/MainActivity.java
@@ -10,7 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_play_game);
-        Intent intent = new Intent(this, PlayGameActivity2.class);
+        Intent intent = new Intent(this, PlayGameActivity.class);
         startActivity(intent);
     }
 }
